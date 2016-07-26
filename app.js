df60c1a8c2ce958b12128b99c10bf2dba40e7135
alexander-r-arnold <alexander.r.arnold@gmail.com>
2016-07-26 17:45:04 -0500

    file creation

---
+++ app.js
@@ -0,0 +1,33 @@
+// require packages
+var express = require('express');
+
+// declare application
+var app = express();
+var port = process.env.PORT || 3000;
+
+// declare route
+var dashboardRouter = require('./src/routes/dashboardRoutes');
+var userRouter = require('./src/routes/userRoutes');
+var aboutRouter = require('./src/routes/aboutRoutes');
+
+// declare session and json parser for the application
+app.use(express.static('public'));
+
+// set views for the ejs engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// map routers
+app.use('/dashboard', dashboardRouter);
+app.use('/user', userRouter);
+app.use('/about', aboutRouter);
+
+// init '/' route
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+// declare application to listen
+app.listen(port, function(err) {
+	console.log('Running server on port ' + port);
+});
