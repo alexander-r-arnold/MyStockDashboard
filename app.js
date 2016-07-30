46fa3e7c078c44834da5e28a6a36bd6ad42acf01
alexander-r-arnold <alexander.r.arnold@gmail.com>
2016-07-30 17:56:40 -0500

    jscs jshint and gulp

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var aboutRouter = require('./src/routes/aboutRoutes')();
 // declare session and json parser for the application
 app.use(express.static(__dirname + 'public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // set views for the ejs engine
 app.set('views', './src/views');
