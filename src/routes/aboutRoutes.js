65935d976034ae57007341537bdfca984ac8c230
alexander-r-arnold <alexander.r.arnold@gmail.com>
2016-08-24 21:37:45 -0400

    endpoint working!

--- src/routes/aboutRoutes.js
+++ src/routes/aboutRoutes.js
@@ -1,16 +1,36 @@
 // require packages
+var request = require('request');
 var express = require('express');
+var bodyParser = require('body-parser');
+var xml2js = require('xml2js');
 var aboutRouter = express.Router();
 
 // handles /about
 var router = function(){
 
-	var stockLookupService = require('../services/stock-lookup-service')();
-	var stockLookupController = require('../controllers/stock-lookup-controller')(stockLookupService);
+	var url = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup?input=';
 
-	aboutRouter.use(stockLookupController.middleware);
-	aboutRouter.route('/') 
-		.get(stockLookupController.getInput);
+	aboutRouter.get('/:symbol', function(req, res) {
+        var symbol = req.params.symbol;
+        request(url + symbol, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                xml2js.parseString(body, function(err, result) {
+                    options = {
+                        symbol: symbol,
+                        resultList: result.LookupResultList.LookupResult
+                    };
+            	res.send(result.LookupResultList.LookupResult);
+            	});
+            }
+        });
+	});
+
+	aboutRouter.get('/', function(req, res) {
+		options = {
+			data: 'no data'
+		}
+		res.render('about', options);
+	}); 
 	
 	return aboutRouter;
 };
